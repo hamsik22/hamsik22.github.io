09ef4ff1e9ba6f4c9286856d043997c3f2e0ffb3
HwangSukHyeon <payback9488@gmail.com>
2024-01-15 07:40:34 +0900

    [+][git]: Add CodingTest
    - 1 Programmer
    -ignore DS_Store

--- CodingTest/CodingTest/Programmers/두 수의 합.swift
+++ CodingTest/CodingTest/Programmers/두 수의 합.swift
@@ -3,6 +3,19 @@
 //  CodingTest
 //
 //  Created by 황석현 on 1/15/24.
-//
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
 
 import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 + num2
+}
+
+//import Foundation
+//
+//func solution(_ num1:Int, _ num2:Int) -> Int {
+//        guard (num1 >= -500000 && num1 <= 500000) , (num2 >= -500000 && num2 <= 500000) else {
+//        return -1
+//    }
+//    return num1 + num2
+//}
