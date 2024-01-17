15c7c1fee8186b5ba2a4eb2382d60a15df547452
HwangSukHyeon <payback9488@gmail.com>
2024-01-17 21:26:00 +0900

    [+][&]: Add CodingTest
    - 2 Programmers
      - 1 solved, 1 In progress

---
+++ CodingTest/CodingTest/Programmers/두 수의 차.swift
@@ -0,0 +1,13 @@
+//
+//  두 수의 차.swift
+//  CodingTest
+//
+//  Created by 황석현 on 1/17/24.
+// 두 수의 차
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 - num2
+}
